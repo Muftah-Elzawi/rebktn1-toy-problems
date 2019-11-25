5c4cbf9e81a9a0d26bdd32767f9e4ff935f57505
muftah <muftah.elzawi@gmail.com>
2019-11-25 10:42:27 +0200

    this.new

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,16 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  var array = string.split('')
+  var arr = []
+  var nonRepeated = ''
+  for (var i = 0; i < array.length; i++) {
+  	arr = array.slice(i)
+  	for (var j = 0; i < arr.length; j++) {
+  		if (array[i] !== arr[j]) {
+			nonRepeated = array[i]
+  		} 
+  	}
+  }
+
 };
