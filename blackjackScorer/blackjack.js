ce81701d52b13c9f84963b839581d7828480c3af
muftah <muftah.elzawi@gmail.com>
2019-11-26 10:16:46 +0200

    finished

--- blackjackScorer/blackjack.js
+++ blackjackScorer/blackjack.js
@@ -13,3 +13,31 @@ Examples
 ["A", "10", "A"]                ==>  12
 ["5", "3", "7"]                 ==>  15
 ["5", "4", "3", "2", "A", "K"]  ==>  25 */
+
+function blackJack(array) {
+
+	// array = argument.split('')
+	var result = 0;
+	array.forEach(function (one,i) {
+		if (one === 'Q' || one === 'K' || one === 'J') {
+			array.splice(i,1,parseInt(10))
+		} 
+		if (one === 'A') {
+			if (array.length < 3) {
+				array.splice(i,1,parseInt(11))
+			}else{
+				array.splice(i,1,parseInt(1))
+			}
+		}
+		
+	})
+	array.forEach(function(one,i){
+		array.splice(i,1,parseInt(one))
+	})
+
+	result = array.reduce(function(elem,acc){
+		return acc += elem	
+	},0)
+	
+	return result
+}
