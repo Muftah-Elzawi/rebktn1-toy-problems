840ae8c48e6560b367e2058a579183c4037e6468
Muftah <muftah.elzawi@gmail.com>
2020-02-20 10:00:26 +0100

    roma

--- RomanNumeralTranslator/RomanNumeralTranslator.js
+++ RomanNumeralTranslator/RomanNumeralTranslator.js
@@ -14,17 +14,54 @@
 
 // You've got Helpers!
 
-// var DIGIT_VALUES = {
-//     I: 1,
-//     V: 5,
-//     X: 10,
-//     L: 50, 
-//     C: 100,
-//     D: 500,
-//     M: 1000
-//   };
+var DIGIT_VALUES = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
 
-function translateRomanNumeral (romanNumeral) {
-    // Write your code here, and
-    // return your final answer.
+function translateRomanNumeral(romanNumeral) {
+  // Write your code here, and
+  // return your final answer.
+  var array = [];
+
+  for (let index = 0; index < romanNumeral.length; index++) {
+    if (romanNumeral[index] === "I") {
+      array.push(DIGIT_VALUES.I);
+    }
+    if (romanNumeral[index] === "V") {
+      array.push(DIGIT_VALUES.V);
+    }
+    if (romanNumeral[index] === "X") {
+      array.push(DIGIT_VALUES.X);
+    }
+    if (romanNumeral[index] === "L") {
+      array.push(DIGIT_VALUES.L);
+    }
+    if (romanNumeral[index] === "C") {
+      array.push(DIGIT_VALUES.C);
+    }
+    if (romanNumeral[index] === "D") {
+      array.push(DIGIT_VALUES.D);
+    }
+    if (romanNumeral[index] === "M") {
+      array.push(DIGIT_VALUES.M);
+    }
   }
+
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] < array[index + 1]) {
+      array[index] = array[index] * -1;
+    }
+  }
+
+  var result = 0;
+  for (let index = 0; index < array.length; index++) {
+    result += array[index];
+  }
+  console.log(result);
+}
