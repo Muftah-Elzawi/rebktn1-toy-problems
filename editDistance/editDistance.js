59d207c3ed447825a72ff272b2d7ddff456b06a1
Muftah <muftah.elzawi@gmail.com>
2020-02-21 09:53:23 +0100

    edit destance

--- editDistance/editDistance.js
+++ editDistance/editDistance.js
@@ -15,5 +15,28 @@ For more information check out this article: https://en.wikipedia.org/wiki/Leven
 */
 
 function editDistance(str1, str2) {
-    // your code here...
+  // your code here...
+  var count = 0;
+  if (str1 === str2) {
+    return "every thing is okay";
+  }
+  if (str2.length === str1.length) {
+    for (let index = 0; index < str1.length; index++) {
+      if (str1[index] !== str2[index]) {
+        str1[index] = str2[index];
+        count++;
+      }
+    }
+  }
+  if (str2.length > str1.length) {
+    for (let index = 0; index < str2.length; index++) {
+      if (str2[index] !== str1[index]) {
+        str1[index] = str2[index];
+        count++;
+      }
+    }
+  }
+  return count;
 }
+
+console.log(editDistance("hackerrank", "hackreactor"));
